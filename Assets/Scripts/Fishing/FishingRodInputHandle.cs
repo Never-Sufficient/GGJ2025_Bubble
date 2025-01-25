b1fef78a856375e86e82b5d81c8377a16a098dca
Phantom <1224983495@qq.com>
2025-01-25 15:30:20 +0800

    钓鱼手感优化

--- Assets/Scripts/Fishing/FishingRodInputHandle.cs
+++ Assets/Scripts/Fishing/FishingRodInputHandle.cs
@@ -5,38 +5,48 @@ using UnityEngine.InputSystem;
 
 public class FishingRodInputHandle : MonoBehaviour
 {
-    [SerializeField] private Transform lineStartPosition;//鱼线顶部
-    [SerializeField] private GameObject hook;//底部鱼钩
-    [SerializeField] private GameObject backGround;//可移动背景
-    [SerializeField] private LineRenderer fishingLine;//鱼线
-    [SerializeField] private float defaultScrollForce;//力的初始值
-    [SerializeField] private float variableScrollForce;//可变化力
-    [SerializeField] private float maxScrollForce;//力的最大值
-    [SerializeField] private float scrollForceGrowingSpeedIntensity;//力的变化速率
-    [SerializeField] private float horizontalMoveIntensity;//水平移动强度
-    [SerializeField] private Transform enterWaterPosition;//下降到水底的指定位置
-    [SerializeField] private Transform exitWaterPosition;//上升到水面的指定位置
+    [SerializeField] private Transform lineStartPosition; //鱼线顶部
+    [SerializeField] private GameObject hook; //底部鱼钩
+    [SerializeField] private GameObject backGround; //可移动背景
+    [SerializeField] private LineRenderer fishingLine; //鱼线
+    [SerializeField] private float defaultScrollForce; //力的初始值
+    [SerializeField] private float variableScrollForce; //可变化力
+    [SerializeField] private float maxScrollForce; //力的最大值
+    [SerializeField] private float scrollForceGrowingSpeedIntensity; //力的变化速率
+    [SerializeField] private float horizontalMoveIntensity; //水平移动强度
+    [SerializeField] private Transform enterWaterPosition; //下降到水底的指定位置
+    [SerializeField] private Transform exitWaterPosition; //上升到水面的指定位置
     [SerializeField] private float drag;
     [SerializeField] private float friction;
-    private InputAction mouseAction;
+    private InputAction hookScrollAction, hookHorizontalMoveAction;
     private float lastVelocity = 0;
-    private bool hookAtTop = false;//鱼钩顶部区域检测
-    private bool hookAtMid = false;//鱼钩中间区域检测
-    private bool hookAtBottom = false;//鱼钩底部区域检测
-    private bool getCollection = false;//钓到物品
-    private bool hookEnterWater = false;//鱼钩正在入水状态检测，须外部初始化
-    private bool hookExitWater = false;//鱼钩正在出水状态检测
+    private bool hookAtTop = false; //鱼钩顶部区域检测
+    private bool hookAtMid = false; //鱼钩中间区域检测
+    private bool hookAtBottom = false; //鱼钩底部区域检测
+    private bool getCollection = false; //钓到物品
+    private bool hookEnterWater = false; //鱼钩正在入水状态检测，须外部初始化
+    private bool hookExitWater = false; //鱼钩正在出水状态检测
     private bool isFishing = false;
-    [SerializeField] private Transform topPoint;//地图的可移动的顶部最大值，须外部初始化
-    [SerializeField] private Transform bottomPoint;//地图的可移动的低部最大值，须外部初始化
-    public float currentDepth { get; private set; }//当前最大深度
-    public float maxDepth { get; private set; }//UI最大深度，须外部初始化
+    [SerializeField] private Transform topPoint; //地图的可移动的顶部最大值，须外部初始化
+    [SerializeField] private Transform bottomPoint; //地图的可移动的低部最大值，须外部初始化
+    public float currentDepth { get; private set; } //当前最大深度
+    public float maxDepth { get; private set; } //UI最大深度，须外部初始化
+
+    private float accumulatedScroll; //当前帧累计滚动距离
+    private float accumulatedMove; //当前帧累计水平移动距离
 
     private void Start()
     {
-        mouseAction = InputSystem.actions.FindAction("MouseOperation");
+        hookScrollAction = InputSystem.actions.FindAction("HookScroll");
+        hookHorizontalMoveAction = InputSystem.actions.FindAction("HookHorizontalMove");
         variableScrollForce = defaultScrollForce;
         setHookEnterWater();
+    }
+
+    private void Update()
+    {
+        accumulatedScroll += hookScrollAction.ReadValue<float>();
+        accumulatedMove += hookHorizontalMoveAction.ReadValue<float>();
     }
 
     private void FixedUpdate()
@@ -44,57 +54,76 @@ public class FishingRodInputHandle : MonoBehaviour
         Debug.Log("getCollection" + getCollection);
         Debug.Log("hookExitWater" + hookExitWater);
         Debug.Log(backGround.transform.position.y > topPoint.transform.position.y);
-        
+
         if (hookEnterWater)
         {
             HookEnterWater();
         }
+
         if (hookExitWater && getCollection)
         {
             HookExitWater();
         }
+
         if (isFishing)
         {
-            Vector2 MouseOperationDir = mouseAction.ReadValue<Vector2>();
+            var hookScroll = accumulatedScroll;
+            var hookHorizontalMove = accumulatedMove;
+            accumulatedScroll = 0;
+            accumulatedMove = 0;
             if (hookAtMid)
             {
                 backGround.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-                HookScroll(MouseOperationDir);
+                HookScroll(hookScroll);
             }
-            if (hookAtBottom && MouseOperationDir.y > 0)
+
+            if (hookAtBottom && hookScroll > 0)
             {
                 backGround.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-                HookScroll(MouseOperationDir);
+                HookScroll(hookScroll);
             }
-            if (hookAtTop && MouseOperationDir.y < 0)
+
+            if (hookAtTop && hookScroll < 0)
             {
                 backGround.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-                HookScroll(MouseOperationDir);
+                HookScroll(hookScroll);
             }
-            if ((hookAtTop && MouseOperationDir.y > 0) || (hookAtBottom && MouseOperationDir.y < 0))
+
+            if ((hookAtTop && hookScroll > 0) || (hookAtBottom && hookScroll < 0))
             {
-                if (backGround.transform.position.y <= topPoint.position.y && backGround.transform.position.y >= bottomPoint.position.y)
+                if (backGround.transform.position.y <= topPoint.position.y &&
+                    backGround.transform.position.y >= bottomPoint.position.y)
                 {
-                    BackGroundScroll(MouseOperationDir);
+                    BackGroundScroll(hookScroll);
                     //currentDepth = Mathf.Abs(bottomPoint.position.y - hook.transform.position.y) / Mathf.Abs(topPoint.position.y - bottomPoint.transform.position.y) * maxDepth;
                 }
             }
-            HorizontalMove(MouseOperationDir);
+
+            HorizontalMove(hookHorizontalMove);
             GetHookPositionOnScreen();
             BackgroundEdgeRegress();
         }
+
         DrawFishingLine();
     }
+
     private void HookEnterWater()
     {
-        transform.position = Vector2.MoveTowards(transform.position, new Vector2(enterWaterPosition.position.x, Camera.main.ScreenToWorldPoint(new Vector3(hook.transform.position.x, Screen.height * 0.666f, hook.transform.position.z)).y), Time.deltaTime * 1.0f);
-        if(Vector2.Distance(new Vector2(enterWaterPosition.position.x, Camera.main.ScreenToWorldPoint(new Vector3(hook.transform.position.x, Screen.height * 0.666f, hook.transform.position.z)).y), transform.position) < 0.01f)
+        transform.position = Vector2.MoveTowards(transform.position,
+            new Vector2(enterWaterPosition.position.x,
+                Camera.main.ScreenToWorldPoint(new Vector3(hook.transform.position.x, Screen.height * 0.666f,
+                    hook.transform.position.z)).y), Time.deltaTime * 1.0f);
+        if (Vector2.Distance(
+                new Vector2(enterWaterPosition.position.x,
+                    Camera.main.ScreenToWorldPoint(new Vector3(hook.transform.position.x, Screen.height * 0.666f,
+                        hook.transform.position.z)).y), transform.position) < 0.01f)
         {
             transform.position = enterWaterPosition.position;
             hookEnterWater = false;
             isFishing = true;
         }
     }
+
     private void HookExitWater()
     {
         isFishing = false;
@@ -106,7 +135,8 @@ public class FishingRodInputHandle : MonoBehaviour
             getCollection = false;
         }
     }
-    private void GetHookPositionOnScreen()//鱼钩屏幕位置检测
+
+    private void GetHookPositionOnScreen() //鱼钩屏幕位置检测
     {
         Vector2 hookPositionOnScreen = Camera.main.WorldToScreenPoint(hook.transform.position);
         if (hookPositionOnScreen.y > Screen.height * 0.666f)
@@ -115,23 +145,30 @@ public class FishingRodInputHandle : MonoBehaviour
             hookAtTop = true;
             hookAtMid = false;
             hookAtBottom = false;
-            hook.transform.position = new Vector3(hook.transform.position.x, Camera.main.ScreenToWorldPoint(new Vector3(hook.transform.position.x, Screen.height * 0.666f, hook.transform.position.z)).y, hook.transform.position.z);
+            hook.transform.position = new Vector3(hook.transform.position.x,
+                Camera.main.ScreenToWorldPoint(new Vector3(hook.transform.position.x, Screen.height * 0.666f,
+                    hook.transform.position.z)).y, hook.transform.position.z);
         }
+
         if (hookPositionOnScreen.y < Screen.height * 0.666f && hookPositionOnScreen.y > Screen.height * 0.333f)
         {
             hookAtTop = false;
             hookAtMid = true;
             hookAtBottom = false;
         }
+
         if (hookPositionOnScreen.y < Screen.height * 0.333f)
         {
             hookAtTop = false;
             hookAtMid = false;
             hookAtBottom = true;
-            hook.transform.position = new Vector3(hook.transform.position.x, Camera.main.ScreenToWorldPoint(new Vector3(hook.transform.position.x, Screen.height * 0.333f, hook.transform.position.z)).y, hook.transform.position.z);
+            hook.transform.position = new Vector3(hook.transform.position.x,
+                Camera.main.ScreenToWorldPoint(new Vector3(hook.transform.position.x, Screen.height * 0.333f,
+                    hook.transform.position.z)).y, hook.transform.position.z);
         }
     }
-    private void BackgroundEdgeRegress()//背景位置回归
+
+    private void BackgroundEdgeRegress() //背景位置回归
     {
         if (backGround.transform.position.y < bottomPoint.transform.position.y)
         {
@@ -139,16 +176,21 @@ public class FishingRodInputHandle : MonoBehaviour
             {
                 hookExitWater = true;
             }
+
             backGround.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            backGround.transform.position = new Vector3(backGround.transform.position.x, bottomPoint.transform.position.y, backGround.transform.position.z);
+            backGround.transform.position = new Vector3(backGround.transform.position.x,
+                bottomPoint.transform.position.y, backGround.transform.position.z);
         }
+
         if (backGround.transform.position.y > topPoint.transform.position.y)
         {
             backGround.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            backGround.transform.position = new Vector3(backGround.transform.position.x, topPoint.transform.position.y, backGround.transform.position.z);
+            backGround.transform.position = new Vector3(backGround.transform.position.x, topPoint.transform.position.y,
+                backGround.transform.position.z);
         }
     }
-    private void HookScroll(Vector2 MouseOperationDir)//鱼钩滚动
+
+    private void HookScroll(float hookScroll) //鱼钩滚动
     {
         Rigidbody2D rb2d = hook.GetComponent<Rigidbody2D>();
         float currentVelocity = rb2d.velocity.normalized.y;
@@ -156,6 +198,7 @@ public class FishingRodInputHandle : MonoBehaviour
         {
             variableScrollForce = defaultScrollForce;
         }
+
         if (rb2d.velocity.normalized.y != 0)
         {
             if (variableScrollForce < maxScrollForce)
@@ -163,12 +206,14 @@ public class FishingRodInputHandle : MonoBehaviour
                 variableScrollForce += scrollForceGrowingSpeedIntensity;
             }
         }
-        rb2d.AddForce(new Vector2(0.0f, MouseOperationDir.y) * variableScrollForce, ForceMode2D.Impulse);
+
+        rb2d.AddForce(new Vector2(0.0f, hookScroll) * variableScrollForce, ForceMode2D.Impulse);
         rb2d.drag = drag;
         rb2d.AddForce(-rb2d.velocity.normalized * friction);
         lastVelocity = currentVelocity;
     }
-    private void BackGroundScroll(Vector2 MouseOperationDir)//背景滚动
+
+    private void BackGroundScroll(float hookScroll) //背景滚动
     {
         Rigidbody2D rb2d = backGround.GetComponent<Rigidbody2D>();
         float currentVelocity = rb2d.velocity.normalized.y;
@@ -176,6 +221,7 @@ public class FishingRodInputHandle : MonoBehaviour
         {
             variableScrollForce = defaultScrollForce;
         }
+
         if (rb2d.velocity.normalized.y != 0)
         {
             if (variableScrollForce < maxScrollForce)
@@ -183,39 +229,48 @@ public class FishingRodInputHandle : MonoBehaviour
                 variableScrollForce += scrollForceGrowingSpeedIntensity;
             }
         }
-        rb2d.AddForce(-new Vector2(0.0f, MouseOperationDir.y) * variableScrollForce, ForceMode2D.Impulse);
+
+        rb2d.AddForce(-new Vector2(0.0f, hookScroll) * variableScrollForce, ForceMode2D.Impulse);
         rb2d.drag = drag;
         rb2d.AddForce(-rb2d.velocity.normalized * friction);
         lastVelocity = currentVelocity;
     }
-    private void HorizontalMove(Vector2 MouseOperationDir)//鱼钩水平移动
+
+    private void HorizontalMove(float hookHorizontalMove) //鱼钩水平移动
     {
         Transform transform = hook.transform;
         //transform.position = new Vector3(transform.position.x + MouseOperationDir.x * horizontalMoveIntensity, transform.position.y, transform.position.z);
-        transform.Translate(MouseOperationDir.x * horizontalMoveIntensity, 0.0f, 0.0f);
+        Debug.Log(hookHorizontalMove);
+        transform.Translate(hookHorizontalMove * horizontalMoveIntensity, 0.0f, 0.0f);
     }
-    private void DrawFishingLine()//画鱼线
+
+    private void DrawFishingLine() //画鱼线
     {
         Vector3[] points = new Vector3[2] { lineStartPosition.position, hook.transform.position };
         fishingLine.positionCount = 2;
         fishingLine.SetPositions(points);
     }
+
     public void setTopPoint(Vector2 Point)
     {
         topPoint.position = Point;
     }
+
     public void setBottomPoint(Vector2 Point)
     {
         bottomPoint.position = Point;
     }
+
     public void setMaxDepth(float depth)
     {
         maxDepth = depth;
     }
+
     public void setGotCollection()
     {
         getCollection = true;
     }
+
     public void setHookEnterWater()
     {
         hookEnterWater = true;
